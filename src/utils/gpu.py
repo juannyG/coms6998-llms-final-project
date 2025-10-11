0f096c0abdb14d9b2da4c0860365b491764d8b46
Juan Gutierrez <jmg2048@columbia.edu>
2025-10-11 08:26:53 -0400

    Add multi-gpu support to utils

--- src/utils/gpu.py
+++ src/utils/gpu.py
@@ -1,32 +1,44 @@
+__all__ = [
+    'gpu_memory_allocated',
+    'reset_peak_mem',
+    'gpu_utilization_percent',
+    'compute_throughput'
+]
+
+from collections import defaultdict
+
 import torch
 import pynvml
 
 pynvml.nvmlInit()
-"""
-TODO: These functions assume 1 GPU.
 
-Needs an update to use torch.cuda.device_count()
-with a slight tweak of the contract (i.e. we return
-a map of gpu_id -> metric)
-"""
 
+def get_gpu_id(i):
+    return f'gpu_{i}'
 
 def gpu_memory_allocated():
     # returns current and peak allocated (MB)
-    if torch.cuda.is_available():
-        cur = torch.cuda.memory_allocated() / (1024**2)
-        peak = torch.cuda.max_memory_allocated() / (1024**2)
-        return cur, peak
-    return 0.0, 0.0
+    gpu_mem_alloc = {get_gpu_id(0): {'cur': 0.0, 'peak': 0.0}}
+    for i in range(torch.cuda.device_count()):
+        with torch.cuda.device(i):
+            gpu_mem_alloc[get_gpu_id(i)] = {
+                'cur': torch.cuda.memory_allocated() / (1024**2),
+                'peak': torch.cuda.max_memory_allocated() / (1024**2),
+            }
+    return gpu_mem_alloc
 
 def reset_peak_mem():
-    if torch.cuda.is_available():
-        torch.cuda.reset_peak_memory_stats()
+    for i in range(torch.cuda.device_count()):
+        with torch.cuda.device(i):
+            torch.cuda.reset_peak_memory_stats()
 
 def gpu_utilization_percent():
-    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
-    util = pynvml.nvmlDeviceGetUtilizationRates(handle)
-    return util.gpu
+    util_perc = {get_gpu_id(0): 0}
+    for i in range(torch.cuda.device_count()):
+        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+        util = pynvml.nvmlDeviceGetUtilizationRates(handle)
+        util_perc[get_gpu_id(i)] = util.gpu
+    return util_perc
 
 def compute_throughput(tokens_processed, seconds):
     return tokens_processed / seconds if seconds > 0 else 0.0
